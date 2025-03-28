32251121a6a6cadd4a7ea8ffb87eac24536567ab
200mill <97425242+200mill@users.noreply.github.com>
2025-03-28 21:53:24 +0900

    timetable X

--- schoolTimeTable.py
+++ schoolTimeTable.py
@@ -1,7 +1,9 @@
 import schoolInfo.py
 import requests
+import datetime
 
 key = ""
+today = datetime.datetime.now().strftime("%Y%m%d")
 
 def schoolTimeTable(school : str, 
                     grade : int,
@@ -9,10 +11,15 @@ def schoolTimeTable(school : str,
     school_info_jsonparsed = schoolInfo.schoolInfo(str(school),)
     if school_info_jsonparsed["SCHUL_KND_SC_NM"] == "초등학교":
         res = requests.get(f'https://open.neis.go.kr/hub/elsTimetable?Type=json&ATPT_OFCDC_SC_CODE={school_info_jsonparsed["ATPT_OFCDC_SC_CODE"]}\
-                           &SD_SCHUL_CODE={school_info_jsonparsed["SD_SCHUL_CODE"]}&key={key}')
+                           &SD_SCHUL_CODE={school_info_jsonparsed["SD_SCHUL_CODE"]}\
+                            &TI_FROM_YMD={today}&TI_TO_YMD={today + 7}&key={key}')
+        res_jsonparsed = res.json()
+        # Timetable
         
         return 0
     elif school_info_jsonparsed["SCHUL_KND_SC_NM"] == "중학교":
+        res = requests.get(f'https://open.neis.go.kr/hub/misTimetable?Type=json&ATPT_OFCDC_SC_CODE={school_info_jsonparsed["ATPT_OFCDC_SC_CODE"]}\
+                           &SD_SCHUL_CODE={school_info_jsonparsed["SD_SCHUL_CODE"]}&key={key}')
         print()
         return 0
     elif school_info_jsonparsed["SCHUL_KND_SC_NM"] == "고등학교":
